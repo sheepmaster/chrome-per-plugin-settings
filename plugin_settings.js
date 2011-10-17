2a4e10dfa6aac80ca7ec6e86dcdce984bdf5c879
Bernhard Bauer <bauerb@google.com>
2011-10-17 14:34:55 +0200

    tmp

--- plugin_settings.js
+++ plugin_settings.js
@@ -1,26 +1,66 @@
 function mockGetContentSettingRules(id) {
   if (id == "adobe-flash-player") {
     return [{
-      'primaryPattern': '*.example.com/*',
-      'secondaryPattern': '*',
-      'setting': 'allow',
+        'primaryPattern': '*.example.com/*',
+        'secondaryPattern': '*',
+        'setting': 'allow',
     }];
   } else {
     return [];
   }
 }
 
-function getContentSettingRules(id, callback) {
-  window.setTimeout(callback, 0, mockGetContentSettingRules(id));
+
+function setContentSetting(id, primaryPattern, secondaryPattern, setting) {
+  chrome.contentSettings.plugins.set({
+      'primaryPattern': primaryPattern,
+      'secondaryPattern': secondaryPattern,
+      'resourceIdentifier': { 'id': id },
+      'setting': setting,
+  }, function() {
+    if (chrome.extension.lastError) {
+      console.error(chrome.extension.lastError.message);
+    } else {
+      window.localStorage.setItem(JSON.stringify([id, primaryPattern,
+                                  secondaryPattern]), setting);
+    }
+  });
 }
 
-function processContentSettingRules(id, rules) {
-  console.log(id + ":" + JSON.stringify(rules));
-  var input = new JsEvalContext({
-    'rules': rules
-  });
-  var output = document.getElementById('plugin-'+id);
-  jstProcess(input, output);
+// function clearContentSetting(id, primaryPattern, secondaryPattern) {
+//   window.localStorage.removeItem(JSON.stringify([id, primaryPattern, secondaryPattern]));
+//   chrome.contentSettings.plugins.set({
+//     'primaryPattern': primaryPattern,
+//     'secondaryPattern': secondaryPattern,
+//     'resourceIdentifier': { 'id': id },
+//     'setting': setting,
+//   }, checkError);
+// }
+
+function getContentSetting(id, primaryPattern, secondaryPattern) {
+  return window.localStorage.getItem(JSON.stringify([id, primaryPattern,
+                                                     secondaryPattern]));
+}
+
+function getContentSettingRules(id) {
+  var length = window.localStorage.length;
+  var rules = [];
+  for (var i = 0; i < length; i++) {
+    var key = window.localStorage.key(i);
+    var keyArray = JSON.parse(key);
+    if (key[0] == id) {
+      rules.push({
+        'primaryPattern': key[1],
+        'secondaryPattern': key[2],
+        'setting': window.localStorage.getItem(key),
+      });
+    }
+  }
+  return rules;
+}
+
+function updateSetting(el) {
+  
 }
 
 function processPlugins(r) {
@@ -43,6 +83,5 @@ function processPlugins(r) {
 }
 
 function init() {
-  chrome.contentSettings.plugins.getResourceIdentifiers(
-      processPlugins);
+  chrome.contentSettings.plugins.getResourceIdentifiers(processPlugins);
 }
